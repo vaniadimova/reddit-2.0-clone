8baeaee36c2602e920f126339e1e2e4a9bf78851
Vania Dimova <ivankadimov8@gmail.com>
2022-05-31 14:34:54 -0400

    apollo client typo

--- apollo/apollo-client.ts
+++ apollo/apollo-client.ts
@@ -4,7 +4,7 @@ export const client = new ApolloClient({
  uri: 'http://localhost:5001/api/fallacious-rabbit',
  cache: new InMemoryCache(),
     headers: {
-        Authorization: `Apikey ${process.env.NEXT_PULIC_STEPZEN_KEY}`
+        Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`
     },
 
 })

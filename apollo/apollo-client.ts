3cedbab39dd84baf54c8703a9b9e20174966d80b
Vania Dimova <ivankadimov8@gmail.com>
2022-05-31 17:34:54 -0400

    using netlify

--- apollo/apollo-client.ts
+++ apollo/apollo-client.ts
@@ -4,7 +4,7 @@ export const client = new ApolloClient({
  uri: 'https://piritiba.stepzen.net/api/fallacious-rabbit/__graphql',
  cache: new InMemoryCache(),
     headers: {
-        Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`
+        Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`,
     },
 
 })
